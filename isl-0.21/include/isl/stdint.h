75419b274ed14a9d2fa73d8245a37d9344e67f90
Sibi <44207430+SibiSiddharthan@users.noreply.github.com>
2019-09-29 12:51:04 +0530

    Initial Commit gmp 90% mpc 100% isl 98% mpfr 95% zlib 100% iconv 80%

---
+++ isl-0.21/include/isl/stdint.h
@@ -0,0 +1,8 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.21"
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
